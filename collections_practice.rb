9c632477ed9f5f9d09c3c0a200c675a748442efa
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-10 23:31:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,8 +8,8 @@ def sort_array_desc(array)
   end
 end 
 
-def sprt_array_char_count(array)
-array.sort do |left, right|
-    left.length <=> right.length
+def sort_array_char_count(array)
+array.sort do |a, b|
+    a.length <=> b.length
   end 
 end 
